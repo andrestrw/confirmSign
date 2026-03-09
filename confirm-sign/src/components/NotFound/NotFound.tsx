c58adbddb6c4a3e78f71790864a2bf1c7977ee1d
Andres Chicaiza <achicaizad@doomity.com>
2026-03-09 21:30:59 +0100

    feat(ui): add NotFound component for unresolved routes

---
+++ confirm-sign/src/components/NotFound/NotFound.tsx
@@ -0,0 +1,12 @@
+import "./NotFound.scss";
+
+const NotFound = () => {
+  return (
+    <div className="not-found-container">
+      <h2>Error 404</h2>
+      <p>No se ha encontrado la ruta. Verifique la URL ingresada.</p>
+    </div>
+  );
+};
+
+export default NotFound;
