306b21c0bdaf3cca1b1003813454a1a3d303cc9b
Andres Chicaiza <achicaizad@doomity.com>
2026-03-17 20:38:25 +0100

    refactor(components): centralize exports to decouple component paths

---
+++ confirm-sign/src/components/index.ts
@@ -0,0 +1,7 @@
+export { default as CfsCodeBar } from "./CfsCodeBar/CfsCodeBar";
+export { default as InfoBar } from "./InfoBar/InfoBar";
+export { default as HistoryTable } from "./HistoryTable/HistoryTable";
+export { default as ContentPanel } from "./ContentPanel/ContentPanel";
+export { default as AgreementForm } from "./Agreement/AgreementForm/AgreementForm";
+export { default as AgreementButton } from "./Agreement/AgreementButton/AgreementButton";
+export { default as HistoryRow } from "./HistoryRow/HistoryRow";
